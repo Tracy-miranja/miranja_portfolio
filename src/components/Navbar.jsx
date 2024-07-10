2005be79e87adc2c6720aac35625202437ae3242
Tracy-miranja <barazatracy44@gmail.com>
2024-07-10 14:52:38 +0300

    Add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "./style";
 import { navLinks } from "../constants";
-import { logo } from "../assets";
+import { logo, menu, close } from "../assets";
 import "./app.css";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -40,6 +41,40 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => {
+              setToggle(!toggle);
+            }}
+          />
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } flex-col p-6 black-gradient top-20 absolute left-0 mx-4 my-2 min-w-[140px] z-20 rounded-xl`}
+          >
+            <ul className="list-none flex flex-col justify-end items-start gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`relative ${
+                    active === link.id ? "text-white" : "text-secondary"
+                  } hover:text-white text-[18px] font-medium cursor-pointer nav-link list-none`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`} className="relative">
+                    {link.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
