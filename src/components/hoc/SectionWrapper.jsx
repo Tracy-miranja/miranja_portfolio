b21786c2696b543abcb9a07e7edcd5273ba8c3b4
Tracy-miranja <barazatracy44@gmail.com>
2024-07-11 23:06:17 +0300

    Create a wrapper section

---
+++ src/components/hoc/SectionWrapper.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { motion } from "framer-motion";
+
+export class SectionWrapper extends Component {
+  render() {
+    return <div>SectionWrapper</div>;
+  }
+}
+
+export default SectionWrapper;
